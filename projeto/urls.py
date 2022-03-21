d0eaaae97a860dfb0fd1f726687ced6b76c675ab
Jamerson Rodrigo <jamesbonovox@hotmail.com>
2022-03-21 19:45:34 -0300

    atualizando

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,7 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse('Home')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('sobre/', sobre),
+    path('contato/', contato)
 ]
